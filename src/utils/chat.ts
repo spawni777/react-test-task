4cee0098c7306b54565b49a132478e5742bb5d8d
Spawni <spawni888@gmail.com>
2023-09-03 19:48:48 +0300

    feat(WebSockets): refactor Socket class a bit to improve readability and maintainability

--- src/utils/chat.ts
+++ src/utils/chat.ts
@@ -13,23 +13,22 @@ const { frontTestServiceBaseURL } = cfg;
 
 export default class Socket {
   url = `ws://${frontTestServiceBaseURL}/chat`;
-  socket: WebSocket;
-  data: SocketData = {};
-  observers: ChatObservers = {
-    close: [],
-    error: [],
-    message: [],
-  };
-
+  socket: WebSocket | null;
+  data: SocketData;
+  observers: ChatObservers;
 
   constructor(topicId: number, url: string = `ws://${frontTestServiceBaseURL}/chat`) {
-    this.data.topicId = topicId;
     this.url = url;
-
-    const { login } = getAuthCookies();
-
-    if (!login) return;
-    this.data.login = login;
+    this.socket = null;
+    this.data = {
+      topicId,
+      login: getAuthCookies().login,
+    };
+    this.observers = {
+      close: [],
+      error: [],
+      message: [],
+    };
   }
 
   requestInitialData() {
@@ -38,17 +37,32 @@ export default class Socket {
       login: this.data.login!,
     };
 
-    this.socket.send(JSON.stringify(initialMessage));
+    this.socket!.send(JSON.stringify(initialMessage));
   }
 
   close() {
-    this.socket.close();
+    this.socket!.close();
   }
 
+  handleInitialMessage(data: string, resolve: (initialData: ChatMessage[]) => void) {
+    const initialData:InitialChatData = JSON.parse(data);
+    const chatMessages = initialData.result.sort((msgA, msgB) => msgA.id - msgB.id);
+
+    this.data.topicName = initialData.topics;
+
+    this.initListeners();
+    resolve(chatMessages);
+  }
+
+  // connect to socket and get initial messages
   async connect() {
     return new Promise((resolve: (initialData: ChatMessage[]) => void, reject) => {
       try {
         this.socket = new WebSocket(this.url);
+
+        ///////////////////////////////////////////////////////////////////
+        /////////////////// close socket on error/close ///////////////////
+        ///////////////////////////////////////////////////////////////////
 
         const initialRejectionListener = () => {
           reject(new Error('WebSocket connection error'))
@@ -62,39 +76,36 @@ export default class Socket {
         ///////////////////////////////////////////////////////////////////
 
         const initialMessageListener = (event) => {
-          this.socket.removeEventListener('message', initialMessageListener);
-          this.socket.removeEventListener('error', initialRejectionListener);
-          this.socket.removeEventListener('close', initialRejectionListener);
-
-          const data = JSON.parse(event.data) as InitialChatData;
-          const chatMessages = data.result.sort((msgA, msgB) => msgA.id - msgB.id);
-
-          this.data.topicName = data.topics;
-
-          this.initListeners();
-          resolve(chatMessages);
-        }
+          this.handleInitialMessage(event.data, resolve);
+          // Remove after first usage
+          this.socket!.removeEventListener('message', initialMessageListener);
+          this.socket!.removeEventListener('error', initialRejectionListener);
+          this.socket!.removeEventListener('close', initialRejectionListener);
+        };
 
         this.socket.addEventListener('message', initialMessageListener);
 
         this.socket.addEventListener('open', () => {
-          console.log('socket connected');
+          console.log('Socket connected');
           this.requestInitialData();
         });
-
       } catch (err) {
         reject(new Error('WebSocket connection error'));
       }
-    })
+    });
   }
 
   initListeners() {
+    if (this.socket === null) return;
+
     this.socket.addEventListener('message', this.onMessageListener);
     this.socket.addEventListener('error', this.onErrorListener);
     this.socket.addEventListener('close', this.onCloseListener);
   }
 
   removeEventListeners() {
+    if (this.socket === null) return;
+
     this.socket.removeEventListener('message', this.onMessageListener);
     this.socket.removeEventListener('error', this.onErrorListener);
     this.socket.removeEventListener('close', this.onCloseListener);
@@ -135,13 +146,6 @@ export default class Socket {
     this.observers[eventName].push(cb);
   }
 
-  // unsubscribe<EventName extends ObserversEventName>(eventName: EventName, cb: ChatObserversMap[EventName]) {
-  //   this.observers[eventName] = this.observers[eventName]
-  //     .filter((observer: ChatObserversMap[EventName]) => {
-  //       return observer !== cb;
-  //     });
-  // }
-
   sendMessage(message: string): ChatMessage {
     const chatMessageRequest: ChatMessageRequest = {
       login: this.data.login!,
@@ -149,7 +153,7 @@ export default class Socket {
       message,
     };
 
-    this.socket.send(JSON.stringify(chatMessageRequest));
+    this.socket!.send(JSON.stringify(chatMessageRequest));
 
     return {
       id: Date.now(),
